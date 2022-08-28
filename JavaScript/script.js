e809e17a6a8625d1b88542bc60c76f18c2ec4502
kauandemello97@gmail.com <kauandemello97@gmail.com>
2022-08-28 19:17:42 -0300

    finalizei receber

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,46 +1,91 @@
-// vars globais:
-let nomeUsuario = "";
-
-// funcao para pegarUsername
-function pegarUsuario(){
-    nomeUsuario = prompt('Qual o usuário?');
-
-    return {
-        name: nomeUsuario,
-    }
-}
-let req = pegarUsuario();
+let nomeUsuario;
 
 
 
-// enviar ao servidor o usuario
+function recebernome(){
+    nomeUsuario = prompt('Qual o seu nome?');
 
-const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', req);
-console.log(promessa);
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', 
+    
+    {
+        name: nomeUsuario
+    });
 
-promessa.then(callback);
-promessa.catch(callbackError);
 
+    promessa.catch(erro);
 
-function callback(resposta){
+};
 
-    if(
-        resposta.status == 200
-    ){('Bem vindo!')}
+recebernome();
+
+function erro(){
+    alert('User já existe!');
+    recebernome();
 }
 
-function callbackError(erro){
-    alert('User já existente!');
-}
 
-// function para manter o servidor
+//function para manter o servidor
 
 setInterval(manterConexao,5000);
 
 function manterConexao(){
-    axios.post('https://mock-api.driven.com.br/api/v6/uol/status', req);
-    console.log('teste')
+    axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nomeUsuario);
 }
 
 
-// pegar mensagens
+
+// receber mensagens
+
+function getM(){
+    const terceiraPromessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+
+    terceiraPromessa.then(renderM);
+}
+
+setInterval(getM, 3000);
+
+function renderM(resposta) {
+    const container = document.querySelector(".container");
+    const mensagens = resposta.data;
+
+    container.innerHTML = "";    
+
+    for (i = 0; i < mensagens.length; i++){
+        if(mensagens[i].type === "status"){
+            container.innerHTML += `
+            <div class="statusM">
+                <div class="time">(${mensagens[i].time})</div>
+                <div class="name">${mensagens[i].from}</div>
+                <div class="text">${mensagens[i].text}</div>
+            </div>`
+        }
+
+        if(mensagens[i].type === "message"){
+            container.innerHTML += `
+            <div class="publicM">
+                    <div class="time">(${mensagens[i].time})</div>
+                    <div class="name">${mensagens[i].from}</div>
+                    para 
+                    <div class="receiver">${mensagens[i].to}</div>
+                    <div class="text">${mensagens[i].text}</div>
+            </div>`
+        }
+
+        if(mensagens[i].type === "private_message" && mensagens[i].to === nameUsuario){
+            container.innerHTML += `
+            <div class="privateM">
+                    <div class="time">(${mensagens[i].time})</div>
+                    <div class="name">${mensagens[i].from}</div>
+                    para 
+                    <div class="receiver">${mensagens[i].to}</div>
+                    <div class="text">${mensagens[i].text}</div>
+            </div>`
+        }
+    }
+    
+    const barraDeRolagem = document.querySelector('.container').lastElementChild
+    barraDeRolagem.scrollIntoView();  
+      
+}
+
+// enviar mensagem 
